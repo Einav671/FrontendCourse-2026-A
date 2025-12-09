5f4ce9737c433e347d6d00ae23dd1940cea8d2b6
Yuval hermon <126855328+cptgingerbeard@users.noreply.github.com>
2025-12-09 10:10:42 +0200

    feat: refactor routing setup

--- candidate-website/src/App.tsx
+++ candidate-website/src/App.tsx
@@ -1,48 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-// ייבוא הרכיבים הקבועים (האדר ופוטר)
-// שים לב: אם בקובץ Header.tsx עשית export default, תוריד את הסוגריים המסולסלים {}
-import Header from "./components/Header"; 
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-// ייבוא הדפים מהתיקייה החדשה שיצרת
+// ייבוא הדפים שיצרנו
 import Home from './pages/Home';
 import Management from './pages/Management';
 import Forms from './pages/Forms';
 
-// ייבוא הפונטים (נשאר כמו במקור)
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './App.css'; // אם יש לך קובץ עיצוב כללי
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        {/* ה-Header מופיע תמיד */}
-        <Header />
-        
-        <main>
-          {/* כאן התוכן מתחלף לפי הכתובת ב-URL */}
-          <Routes>
-            {/* דף הבית */}
-            <Route path="/" element={<Home />} />
-            
-            {/* דף הניהול (שמכיל בתוכו את CoursesTable) */}
-            <Route path="/management" element={<Management />} />
-            
-            {/* דף הטפסים */}
-            <Route path="/forms" element={<Forms />} />
-          </Routes>
-        </main>
+    <div className="app-container">
+      <Header />
+      
+      <main>
+        {/* הגדרת נתיב (Route) לכל קומפוננטה חדשה */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/management" element={<Management />} />
+          <Route path="/forms" element={<Forms />} />
+        </Routes>
+      </main>
 
-        {/* ה-Footer מופיע תמיד */}
-        <Footer />
-      </div>
-    </Router>
+      <Footer />
+    </div>
   );
 }
 
-export default App;
+export default App
