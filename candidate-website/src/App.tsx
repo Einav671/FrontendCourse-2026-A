d8fb245fb059e2be3be3c8d907e628fdc67bf9a7
Einav Deri <derieinav0@gmail.com>
2025-12-11 13:17:31 +0200

    Added Help page

--- candidate-website/src/App.tsx
+++ candidate-website/src/App.tsx
@@ -1,36 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
-
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-// ייבוא הדפים שיצרנו
+// import Footer from "./components/Footer"; // נשאיר בהערה אם עדיין אין לכם פוטר
 import Home from './pages/Home';
 import Management from './pages/Management';
 import Forms from './pages/Forms';
+import Help from './pages/Help';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
       <Header />
-      
       <main>
-        {/* הגדרת נתיב (Route) לכל קומפוננטה חדשה */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/management" element={<Management />} />
           <Route path="/forms" element={<Forms />} />
+          <Route path="/help" element={<Help />} />
         </Routes>
       </main>
-
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
 
-export default App
+export default App;
--- candidate-website/src/components/Header.tsx
+++ candidate-website/src/components/Header.tsx
@@ -3,27 +3,21 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom'; // ניווט ללא רענון דף
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        {/* שם האתר / לוגו */}
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          מערכת ניהול קורסים
+          מערכת רישום
         </Typography>
 
-        {/* כפתורי ניווט */}
-        <Button color="inherit" component={Link} to="/">
-          בית
-        </Button>
-        <Button color="inherit" component={Link} to="/management">
-          ניהול (Management)
-        </Button>
-        <Button color="inherit" component={Link} to="/forms">
-          טפסים (Forms)
-        </Button>
+        <Button color="inherit" component={Link} to="/">בית</Button>
+        <Button color="inherit" component={Link} to="/management">ניהול</Button>
+        <Button color="inherit" component={Link} to="/forms">טפסים</Button>
+        {/* הכפתור שלך */}
+        <Button color="inherit" component={Link} to="/help">עזרה</Button>
       </Toolbar>
     </AppBar>
   );
